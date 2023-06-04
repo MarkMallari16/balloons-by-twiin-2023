e8787a0aa54db8bb447f1b0b881b011af0c4441a
Mark Mallari <mallariftw01@gmail.com>
2023-06-04 15:16:29 +0800

    commit

---
+++ formValidation.js
@@ -0,0 +1,6 @@
+var firstName = document.getElementById("firstName");
+var lastName = document.getElementById("lastName");
+
+document.getElementById("submit").addEventListener("click", () => {
+    validate();
+});
