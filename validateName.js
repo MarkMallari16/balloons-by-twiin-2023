446edb851e6423b5fc0aa2747fee77b5ea45819b
Mark Mallari <mallariftw01@gmail.com>
2023-06-06 10:48:22 +0800

    commit

---
+++ validateName.js
@@ -0,0 +1,8 @@
+var fnameInput = document.getElementById("firstName");
+var lnameInput = document.getElementById("lastName");
+
+var firstName = fnameInput.value;
+var lastName = lnameInput.value;
+
+var pattern = /^[a-zA-Z\s]+$/;
+
