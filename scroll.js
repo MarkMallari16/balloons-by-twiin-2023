fb297c03bf72d51df2a7c480bab4be2ed69e0a13
Mark Christian Mallari <104248911+MarkMallari16@users.noreply.github.com>
2025-05-24 22:12:48 +0800

    Delete scroll.js

--- scroll.js
+++
@@ -1,9 +0,0 @@
-const toTop = document.querySelector(".button-up");
-
-window.addEventListener("scroll" ,() => {
-    if(window.pageYOffset>100){
-        toTop.classList.add("active");
-    }else{
-        toTop.classList.remove("active");
-    }
-})
